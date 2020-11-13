532f564d6e3e35a6dd65b9c4142f0812c2abd2bd
leunghb <891547988@qq.com>
2020-11-13 10:07:56 +0800

    搜索完成

--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="search fullscreen">
+    <div class="search">
         <div class="header">
             <van-icon class="backIcon" name="arrow-left" @click="back()" />
             <van-search
                 class="search"
                 v-model="searchValue"
                 show-action
-                placeholder="请输入搜索关键词"
+                placeholder="请输入搜索商品名称"
                 @search="search"
+                @clear="clear"
+                onkeyup="this.value=this.value.replace(/^\s+|\s+$/g,'')"
             >
                 <template #action>
                     <div @click="search">搜索</div>
@@ -19,15 +21,41 @@
             class="list"
             v-model="loading"
             :finished="finished"
-            finished-text="没有更多了"
+            :finished-text="finishedText"
             @load="onLoad"
         >
-            <van-cell v-for="item in list" :key="item" :title="item" />
+            <van-cell class="item" v-for="item in list" :key="item.id">
+                <img class="cover" :src="item.cover" v-lazy="item.cover" alt />
+                <div
+                    class="mainTitle multipleLinesOmitted"
+                    v-text="item.mainTitle"
+                ></div>
+                <div class="price">
+                    <div
+                        :class="[
+                            'originalPrice',
+                            item.discountPrice > 0 ? 'lineThrough' : '',
+                        ]"
+                        v-text="'￥' + item.originalPrice"
+                    >
+                        ￥112.00
+                    </div>
+                    <div
+                        class="discountPrice"
+                        v-if="item.discountPrice > 0"
+                        v-text="'￥' + item.discountPrice"
+                    >
+                        ￥11.11
+                    </div>
+                </div>
+            </van-cell>
         </van-list>
     </div>
 </template>
 
 <script>
+import { post, api } from "../../utils/httpApi";
+
 export default {
     data() {
         return {
@@ -36,26 +64,65 @@ export default {
             list: [],
             loading: false,
             finished: false,
+            finishedText: "",
+            page: 0,
+            limit: 10,
         };
     },
     methods: {
-        search() {},
+        search() {
+            this.finished = false;
+            this.page = 0;
+            this.list = [];
+            this.onLoad();
+        },
+        clear() {
+            this.finished = false;
+            this.loading = false;
+            this.page = 0;
+            this.list = [];
+        },
         onLoad() {
-            // 异步更新数据
-            // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-            setTimeout(() => {
-                for (let i = 0; i < 10; i++) {
-                    this.list.push(this.list.length + 1);
-                }
-
-                // 加载状态结束
-                this.loading = false;
-
-                // 数据全部加载完成
-                if (this.list.length >= 40) {
-                    this.finished = true;
-                }
-            }, 1000);
+            if (this.searchValue.length == 0) {
+                this.finishedText = "";
+                this.finished = true;
+                return false;
+            }
+            ++this.page;
+            let params = this.$qs.stringify({
+                limit: this.limit,
+                page: this.page,
+                goodsTypeId: this.currentClassifyId,
+                soldOut: 0,
+                mainTitle: this.searchValue,
+            });
+            post(api.getGoods, params)
+                .then((res) => {
+                    let data = res.data;
+                    console.log(data);
+                    if (data.data.length == 0) {
+                        this.finishedText = "换个商品吧 _(:з」∠)_";
+                        this.finished = true;
+                        return false;
+                    }
+                    if (data.code == 0) {
+                        data.data.forEach((v) => {
+                            v.cover = api.baseUrl + v.cover.split(",")[0];
+                            this.list.push(v);
+                        });
+                        this.loading = false;
+                        if (
+                            this.list.length < this.limit ||
+                            this.list.length % this.limit != 0
+                        ) {
+                            this.finished = true;
+                            this.finishedText = "这是全部了 (..•˘_˘•..)";
+                        }
+                    }
+                })
+                .catch((err) => {
+                    this.$toast(err);
+                });
         },
         back() {
             this.$router.go(-1);
