09533e5c7159812720176bd15757fdca42d42946
leunghb <891547988@qq.com>
2020-11-16 17:24:59 +0800

    商品详情ui

--- src/pages/goodsDetail/goodsDetail.vue
+++ src/pages/goodsDetail/goodsDetail.vue
@@ -21,15 +21,34 @@
             </div>
             <div class="subTitle">纯天然产品</div>
             <div class="priceAndsalesVolume">
-                <div class="noDiscount">
-                    <span>￥</span>50.00
-                </div>
+                <div class="noDiscount"><span>￥</span>50.00</div>
                 <!-- <div class="haveDiscount">
                     <span>￥</span>50.00
                     <span>价格￥11.00</span>
                 </div> -->
                 <div class="salesVolume">已售1份</div>
             </div>
+        </div>
+        <div class="specifications">
+            <div class="label">选择</div>
+            <van-icon name="arrow" />
+            <div class="value">规格</div>
+        </div>
+        <div class="address">
+            <div class="label">地址</div>
+            <van-icon name="arrow" />
+            <div class="value">厦门</div>
+        </div>
+        <div class="evaluate">
+            <div class="label">评价</div>
+            <van-icon name="arrow" />
+            <div class="value"></div>
+        </div>
+        <div class="detail">详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情</div>
+        
+        <div class="footer">
+            <div class="buy">购买</div>
+            <div class="cart">加入购物车</div>
         </div>
     </div>
 </template>
