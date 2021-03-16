cf58ec6aaf0a546aa3b14aa9e0d1d90e96066e19
lhb <891547988@qq.com>
2021-03-16 21:13:26 +0800

    退出登录弹出提示；用户头像显示错误使用默认头像；

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -2,31 +2,32 @@
     <div class="my fullscreen">
         <div class="header">
             <img
-                    @click="updateAvatar"
-                    class="avatar"
-                    :src="avatar != '' ? avatar : defaultAvatar"
-                    alt=""
-                    srcset=""
+                @click="updateAvatar"
+                class="avatar"
+                :src="avatar"
+                alt=""
+                srcset=""
+                :onerror="errorAvatar"
             />
             <input
-                    ref="avatarInput"
-                    v-show="false"
-                    type="file"
-                    @change="selAvatar($event)"
+                ref="avatarInput"
+                v-show="false"
+                type="file"
+                @change="selAvatar($event)"
             />
             <div
-                    class="name"
-                    v-text="name != '' ? name : account"
-                    @click="showChangeNamePopup = true"
+                class="name"
+                v-text="name != '' ? name : account"
+                @click="showChangeNamePopup = true"
             ></div>
-            <van-icon name="close" class="logout" @click="logout()"/>
+            <van-icon name="close" class="logout" @click="logout()" />
             <van-popup class="changeNamePopup" v-model="showChangeNamePopup">
                 <input
-                        type="text"
-                        placeholder="请输入名称"
-                        v-model="newName"
-                        onkeyup="this.value=this.value.replace(/^\s+|\s+$/g,'')"
-                        maxlength="6"
+                    type="text"
+                    placeholder="请输入名称"
+                    v-model="newName"
+                    onkeyup="this.value=this.value.replace(/^\s+|\s+$/g,'')"
+                    maxlength="6"
                 />
                 <div class="changeNameBtn" @click="changeName">确认修改</div>
             </van-popup>
@@ -36,118 +37,127 @@
             <div class="item" @click="toPage('Balance')">
                 <i class="iconfont iconyue"></i>
                 <div class="label">余额</div>
-                <van-icon name="arrow"/>
+                <van-icon name="arrow" />
             </div>
             <div class="item" @click="toPage('Order')">
                 <i class="iconfont icondingdanyichenggong"></i>
                 <div class="label">订单</div>
-                <van-icon name="arrow"/>
+                <van-icon name="arrow" />
             </div>
             <div class="item" @click="toPage('Collect')">
                 <i class="iconfont iconshoucang"></i>
                 <div class="label">收藏</div>
-                <van-icon name="arrow"/>
+                <van-icon name="arrow" />
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import {api, get, post} from "../../utils/httpApi";
+import { api, get, post } from "../../utils/httpApi";
 
-    export default {
-        data() {
-            return {
-                name: "",
-                account: "",
-                avatar: "",
-                defaultAvatar: require("../../assets/default-avatar.png"),
+export default {
+    data() {
+        return {
+            name: "",
+            account: "",
+            avatar: "",
+            defaultAvatar: require("../../assets/default-avatar.png"),
+            errorAvatar:
+                "this.src='" + require("../../assets/default-avatar.png") + "'",
 
-                showChangeNamePopup: false,
-                newName: "",
-            };
-        },
-        created() {
-            this.getUserInfo();
-        },
-        methods: {
-            getUserInfo() {
-                get(api.getUserInfo)
-                    .then((res) => {
-                        let data = res.data.data;
-                        this.name = data.name;
-                        this.account = data.account;
-                        this.avatar =
-                            data.avatar != "" ? api.baseUrl + data.avatar : "";
-                    })
-                    .catch((err) => {
-                        this.$toast(err.messgae);
-                    });
-            },
-            selAvatar(e) {
-                let files = e.target.files[0];
-                let params = new FormData();
-                params.append("file", files);
-                params.append("fileType", 0);
-                post(api.uploadSinglePicture, params)
-                    .then((res) => {
-                        let data = res.data;
-                        if (data.code == 0) {
-                            this.putUserAvatarOrName(data.data, this.name);
-                            return false;
-                        }
-                        this.$toast("头像上传失败");
-                    })
-                    .catch((err) => {
-                        this.$toast(err.messgae);
-                    });
-            },
-            updateAvatar() {
-                this.$refs.avatarInput.click();
-            },
-            putUserAvatarOrName(avatar, name) {
-                let params = this.$qs.stringify({
-                    avatar: avatar,
-                    name: name,
-                });
-                post(api.putUserAvatarOrName, params)
-                    .then((res) => {
-                        let data = res.data;
-                        if (data.code == 0) {
-                            this.$toast("更新成功");
-                            this.avatar = api.baseUrl + avatar;
-                            this.name = name;
-                            this.showChangeNamePopup = false;
-                            return false;
-                        }
-                        this.$toast("更新失败");
-                    })
-                    .catch((err) => {
-                        this.$toast(err.messgae);
-                    });
-            },
-            changeName() {
-                let avatar = this.avatar.replace(api.baseUrl, ""),
-                    name = this.newName;
-                this.putUserAvatarOrName(avatar, name);
-            },
-            toPage(page) {
-                this.$router.push({
-                    path: "/" + page,
-                });
-            },
-            logout() {
-                localStorage.clear();
-                this.$router.replace({
-                    path: "/Login"
+            showChangeNamePopup: false,
+            newName: "",
+        };
+    },
+    created() {
+        this.getUserInfo();
+    },
+    methods: {
+        getUserInfo() {
+            get(api.getUserInfo)
+                .then((res) => {
+                    let data = res.data.data;
+                    this.name = data.name;
+                    this.account = data.account;
+                    this.avatar = data.avatar
+                        ? api.baseUrl + data.avatar
+                        : this.defaultAvatar;
                 })
-            }
+                .catch((err) => {
+                    this.$toast(err.messgae);
+                });
         },
-    };
+        selAvatar(e) {
+            let files = e.target.files[0];
+            let params = new FormData();
+            params.append("file", files);
+            params.append("fileType", 0);
+            post(api.uploadSinglePicture, params)
+                .then((res) => {
+                    let data = res.data;
+                    if (data.code == 0) {
+                        this.putUserAvatarOrName(data.data, this.name);
+                        return false;
+                    }
+                    this.$toast("头像上传失败");
+                })
+                .catch((err) => {
+                    this.$toast(err.messgae);
+                });
+        },
+        updateAvatar() {
+            this.$refs.avatarInput.click();
+        },
+        putUserAvatarOrName(avatar, name) {
+            let params = this.$qs.stringify({
+                avatar: avatar,
+                name: name,
+            });
+            post(api.putUserAvatarOrName, params)
+                .then((res) => {
+                    let data = res.data;
+                    if (data.code == 0) {
+                        this.$toast("更新成功");
+                        this.avatar = api.baseUrl + avatar;
+                        this.name = name;
+                        this.showChangeNamePopup = false;
+                        return false;
+                    }
+                    this.$toast("更新失败");
+                })
+                .catch((err) => {
+                    this.$toast(err.messgae);
+                });
+        },
+        changeName() {
+            let avatar = this.avatar.replace(api.baseUrl, ""),
+                name = this.newName;
+            this.putUserAvatarOrName(avatar, name);
+        },
+        toPage(page) {
+            this.$router.push({
+                path: "/" + page,
+            });
+        },
+        logout() {
+            this.$dialog
+                .confirm({
+                    title: "确认退出？",
+                })
+                .then(() => {
+                    localStorage.clear();
+                    this.$router.replace({
+                        path: "/Login",
+                    });
+                });
+        },
+    },
+};
 </script>
 
 <style lang="scss" scoped>
-    @import "../../static/css/common.scss";
-    @import "../../static/css/iconfont.css";
-    @import "./my.scss";
+@import "../../static/css/common.scss";
+@import "../../static/css/iconfont.css";
+@import "./my.scss";
 </style>
